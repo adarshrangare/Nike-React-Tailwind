4a4e0799bfaf1f6310371c196655ba88b495327d
adarshrangare <akrangare@gmail.com>
2023-11-17 00:31:06 +0530

    2nd Commit

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -5,7 +5,7 @@ import { footerLinks, socialMedia } from "../constants"
 const Footer = () => {
   return (
     <footer
-    className="max-container"
+    className="max-container" id="contact-us"
     >
       <div
       className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col "
